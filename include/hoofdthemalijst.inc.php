8e305c0d2e900b854663be15cc650af1f02ee476
Yilmaz Büyükleyla <00312238@rijnijssel.nl>
2023-05-08 12:00:02 +0200

    hoofdthema add

---
+++ include/hoofdthemalijst.inc.php
@@ -0,0 +1,148 @@
+<?php if (isset($_SESSION['userrol'])) { // check if user is logedin ?>
+  <div class="beewaylijst">
+      <?php if ($_SESSION['userrol'] == "superuser") { ?>
+        <div class="beewaylijsttitel"><h1>Welkom op het super user dashboard</h1></div>
+        <h2>beheer hier dingen (:</h2>
+
+        <div class="beewaylijstopties">
+          <button onclick="window.location.href='index.php?page=userlijst';" id="beewaylijstopties5">Users</button>
+          <b>|</b>
+          <button onclick="window.location.href='index.php?page=scholenlijst';" id="beewaylijstopties5"><u>Scholen</u></button>
+          <b>|</b>
+          <button onclick="window.location.href='index.php?page=logslijst';" id="beewaylijstopties5">Site Logs</button>
+      <?php } else if ($_SESSION['userrol'] == "admin") {?>
+        <div class="beewaylijsttitel"><h1>Welkom op het admin dashboard</h1></div>
+        <h2>beheer hier dingen (:</h2>
+
+        <div class="beewaylijstopties">
+          <button onclick="window.location.href='index.php?page=beewaylijst';" id="beewaylijstopties1">Beeway's</button>
+          <b>|</b>
+          <button onclick="window.location.href='index.php?page=klassenlijst';" id="beewaylijstopties4">Klassen</button>
+          <b>|</b>
+          <button onclick="window.location.href='index.php?page=vakkenlijst';" id="beewaylijstopties2">Vakken</button>
+          <b>|</b>
+          <button onclick="window.location.href='index.php?page=Hoofdthemalijst';" id="beewaylijstopties3"><u>Hoofdthema's</u></button>
+          <b>|</b>
+          <button onclick="window.location.href='index.php?page=userlijst';" id="beewaylijstopties5">Users</button>
+      <?php } else { ?>
+        <div class="beewaylijsttitel"><h1>Welkom op het docenten dashboard</h1></div>
+        <h2>beheer hier dingen (:</h2>
+
+        <div class="beewaylijstopties">
+          <button onclick="window.location.href='index.php?page=beewaylijst';" id="beewaylijstopties1">Beeway's</button>
+      <?php } ?>
+    </div>
+
+    <hr>
+
+    <br>
+
+      <?php
+      $sql = 'SELECT schoolid FROM users
+              WHERE userid= :userid';
+      $sth = $conn->prepare($sql);
+      $sth->bindParam(':userid', $_SESSION['userid']);
+      $sth->execute();
+      while ($school = $sth->fetch(PDO::FETCH_OBJ)) {
+        $schoolid = $school -> schoolid;
+
+      }
+
+        if (isset($_GET['offset'])) {
+          $offset = $_GET['offset'] * 4;
+
+
+        } else {
+          $sql = 'SELECT * FROM maintheme
+                  WHERE schoolid = :schoolid
+                  LIMIT 4';
+          $sth = $conn->prepare($sql);
+          $sth->bindParam(':schoolid', $schoolid);
+          $sth->execute();
+        }
+
+        if ($sth->rowCount() > 0) {
+          echo '<table class="beewaylijsttable">
+            <tr>
+              <th><h3>Schooljaar</h3></th>
+              <th><h3>Periode 1</h3></th>
+              <th><h3>Periode 2</h3></th>
+              <th><h3>Periode 3</h3></th>
+              <th><h3>Periode 4</h3></th>
+              <th><h3>Periode 5</h3></th>
+              <th><h3>verwijderd</h3></th>
+              <th><a href="index.php?page=hoofdthematoevoegen" class="addbutton">toevoegen</a></th>
+            </tr>';
+          while ($maintheme = $sth->fetch(PDO::FETCH_OBJ)) {
+            if ($maintheme->archive == "1") {$archive = "yes";}
+            else {$archive = "no";}
+
+            if ($maintheme->schoolyear == "1") {$schoolyear = "2021/2022";}
+            else if ($maintheme->schoolyear == "2") {$schoolyear = "2022/2023";}
+            else if ($maintheme->schoolyear == "3") {$schoolyear = "2023/2024";}
+            else if ($maintheme->schoolyear == "4") {$schoolyear = "2024/2025";}
+            else if ($maintheme->schoolyear == "5") {$schoolyear = "2025/2026";}
+            else if ($maintheme->schoolyear == "6") {$schoolyear = "2026/2027";}
+            else if ($maintheme->schoolyear == "7") {$schoolyear = "2027/2028";}
+
+
+            echo'
+              <tr>
+                <td><b>'.$schoolyear.'</b></td>
+                <td><b>'.$maintheme->namethemep1.'</b></td>
+                <td><b>'.$maintheme->namethemep2.'</b></td>
+                <td><b>'.$maintheme->namethemep3.'</b></td>
+                <td><b>'.$maintheme->namethemep4.'</b></td>
+                <td><b>'.$maintheme->namethemep5.'</b></td>
+                <td><b>'.$archive.'</b></td>
+                <td><a href="schoolaanpassen.html" class="editbutton">bewerken</a></td>
+              </tr>
+            ';
+
+          }
+          echo '</table>
+
+
+          <div class="tablebuttons">';
+            if (isset($_GET['offset'])) {
+              $terug = $_GET['offset'] - 1;
+              $volgende = $_GET['offset'] + 1;
+              if ($_GET['offset'] == '0') {
+                // echo '
+                //   <a href="index.php?page=scholenlijst&offset='.$volgende.'" class="addbutton">volgende</a>
+                // ';
+              } else {
+                // echo '
+                //   <a href="index.php?page=scholenlijst&offset='.$terug.'" class="addbutton">terug</a>
+                //   <a href="index.php?page=scholenlijst&offset='.$volgende.'" class="addbutton">volgende</a>
+                // ';
+              }
+            } else {
+              // echo '
+              //   <a href="index.php?page=scholenlijst&offset=1" class="addbutton">volgende</a>
+              // ';
+            }
+          echo '</div>';
+        } else {
+          // the query did not return any rows
+          echo '<h2><strong>the query did not return any rows</string></h2>';
+          if (isset($_GET['offset']) && $_GET['offset'] >= '1') {
+            $terug = $_GET['offset'] - 1;
+
+            echo '<div class="tablebuttons"><a href="index.php?page=scholenlijst&offset='.$terug.'" class="addbutton">terug</a></div>';
+          } else if (isset($_GET['offset'])) {
+            echo '<div class="tablebuttons"><a href="index.php?page=scholenlijst" class="addbutton">terug</a></div>';
+          }
+          $_SESSION['error'] = "the query did not return any rows. Pech!";
+        }
+      ?>
+
+
+    <hr>
+  </div>
+
+  <?php include 'include/error.inc.php'; ?>
+<?php } else {
+  $_SESSION['error'] = "er ging iets mis. Pech!";
+  header("location: index.php?page=login");
+} ?>
---
+++ include/hoofdthematoevoegen.inc.php
@@ -0,0 +1,40 @@
+<?php  ?>
+
+<div class="addedit">
+    <form class="form" action="#" method="POST">
+      <div id="name"><h1>Hoofdthema toevoegen</h1>
+      <p>Voeg een nieuwe Hoofdthema toe aan het systeem</p></div>
+      <hr style="margin: 20px 0;">
+      <div id="LP">
+        <label for="NaamThemaP1"><b>Hoofdthema naam Periode 1</b></label>
+        <input id="textaddedit" type="text" placeholder="NaamThemaP1" name="NaamThemaP1" required>
+        <label for="NaamThemaP2"><b>Hoofdthema naam Periode 2</b></label>
+        <input id="textaddedit" type="text" placeholder="NaamThemaP2" name="NaamThemaP2" required>
+        <label for="NaamThemaP3"><b>Hoofdthema naam Periode 3</b></label>
+        <input id="textaddedit" type="text" placeholder="NaamThemaP3" name="NaamThemaP3" required>
+      </div>
+      <div id="RP">
+        <label for="NaamThemaP4"><b>Hoofdthema naam Periode 4</b></label>
+        <input id="textaddedit" type="text" placeholder="NaamThemaP4" name="NaamThemaP4" required>
+        <label for="NaamThemaP5"><b>Hoofdthema naam Periode 5</b></label>
+        <input id="textaddedit" type="text" placeholder="NaamThemaP5" name="NaamThemaP5" required>
+
+        <label for="Schooljaar"><b>Schooljaar</b></label>
+        <select id="selectaddedit" name="Schooljaar" >
+          <optgroup label="Selecteer een Schooljaar">
+            <option type="dropdown" placeholder="Schooljaar" value="1">2021/2022</option>
+            <option type="dropdown" placeholder="Schooljaar" value="2" selected>2022/2023</option>
+            <option type="dropdown" placeholder="Schooljaar" value="3">2023/2024</option>
+            <option type="dropdown" placeholder="Schooljaar" value="4">2024/2025</option>
+            <option type="dropdown" placeholder="Schooljaar" value="5">2025/2026</option>
+            <option type="dropdown" placeholder="Schooljaar" value="6">2026/2027</option>
+          </optgroup>
+        </select>
+      </div>
+
+      <div id="errormsg"></div>
+
+      <hr style="margin: 20px 0;">
+      <button type="submit" class="addbutton" style="font-size:20px;font-weight: bold;">Hoofdthema toevoegen</button>
+    </form>
+  </div>
